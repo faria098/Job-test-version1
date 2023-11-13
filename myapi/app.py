925e22bca8c24c78b056fa70755083ecacb967da
faria098 <150492226+faria098@users.noreply.github.com>
2023-11-12 23:55:30 -0600

    endpoint for new alert

--- myapi/app.py
+++ myapi/app.py
@@ -18,3 +18,24 @@ def init_db():
 
 init_db()
 
+
+
+# Route for creating a new alert
+@app.route('/alerts', methods=['POST'])
+def create_alert():
+    data = request.get_json()
+    message = data.get('message')
+
+    if not message:
+        return jsonify({'error': 'Message is required'}), 400
+
+    conn = sqlite3.connect('myapi/database/alerts.db')
+    cursor = conn.cursor()
+
+    cursor.execute('INSERT INTO alerts (message) VALUES (?)', (message,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Alert created successfully'}), 201
+
+
