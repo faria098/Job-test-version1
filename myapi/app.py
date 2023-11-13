0c9982e91e936d46237d131ae577a31c6bc5a7b9
faria098 <150492226+faria098@users.noreply.github.com>
2023-11-13 00:06:57 -0600

    added all alerts

--- myapi/app.py
+++ myapi/app.py
@@ -39,3 +39,24 @@ def create_alert():
     return jsonify({'message': 'Alert created successfully'}), 201
 
 
+
+# Route for retrieving all alerts
+@app.route('/alerts', methods=['GET'])
+def get_alerts():
+    conn = sqlite3.connect('myapi/database/alerts.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM alerts ORDER BY timestamp DESC')
+    alerts = cursor.fetchall()
+
+    conn.close()
+
+    alert_list = [{'id': alert[0], 'message': alert[1], 'timestamp': alert[2]} for alert in alerts]
+
+    return jsonify(alert_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
