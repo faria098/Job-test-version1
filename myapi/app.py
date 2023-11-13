56ad4b7842edcf37e24891c71215985a3bdbc1b0
faria098 <150492226+faria098@users.noreply.github.com>
2023-11-13 00:40:54 -0600

    fixing database address, added endpoint by id

--- myapi/app.py
+++ myapi/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Initialize the database connection
 def init_db():
-    conn = sqlite3.connect('myapi/database/alerts.db')
+    conn = sqlite3.connect('database/alerts.db')
     conn.execute('''
         CREATE TABLE IF NOT EXISTS alerts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,7 +29,7 @@ def create_alert():
     if not message:
         return jsonify({'error': 'Message is required'}), 400
 
-    conn = sqlite3.connect('myapi/database/alerts.db')
+    conn = sqlite3.connect('database/alerts.db')
     cursor = conn.cursor()
 
     cursor.execute('INSERT INTO alerts (message) VALUES (?)', (message,))
@@ -43,7 +43,7 @@ def create_alert():
 # Route for retrieving all alerts
 @app.route('/alerts', methods=['GET'])
 def get_alerts():
-    conn = sqlite3.connect('myapi/database/alerts.db')
+    conn = sqlite3.connect('database/alerts.db')
     cursor = conn.cursor()
 
     cursor.execute('SELECT * FROM alerts ORDER BY timestamp DESC')
@@ -55,8 +55,29 @@ def get_alerts():
 
     return jsonify(alert_list)
 
+# Route for retrieving a specific alert by ID
+@app.route('/alerts/<int:alert_id>', methods=['GET'])
+def get_alert_by_id(alert_id):
+    conn = sqlite3.connect('database/alerts.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM alerts WHERE id = ?', (alert_id,))
+    alert = cursor.fetchone()
+
+    conn.close()
+
+    if alert is None:
+        return jsonify({'error': 'Alert not found'}), 404
+
+    alert_data = {
+        'id': alert[0],
+        'message': alert[1],
+        'timestamp': alert[2]
+    }
+
+    return jsonify(alert_data)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-    
